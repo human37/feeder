5f2f95f3eccd65f77b1176ef76fec3a48db36a12
Ammon Taylor <ammonx9@gmail.com>
2021-06-29 12:36:42 -0600

    fixed initial-scale

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     config.plugin("html").tap((args) => {
       args[0].title = "simple feeder";
       args[0].meta = {
-        viewport: "width=device-width, initial-scale=1.25, maximum-scale=2, user-scalable=no, minimal-ui",
+        viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui",
       };
 
       return args;
