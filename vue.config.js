af0171271ecfa62f30c18490ae6b6a33f066ada3
Ammon Taylor <ammonx9@gmail.com>
2021-06-29 12:18:26 -0600

    added a viewport

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+  transpileDependencies: ["vuetify"],
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "simple feeder";
+      args[0].meta = {
+        viewport: "width=device-width, initial-scale=1.25, maximum-scale=2, user-scalable=no, minimal-ui",
+      };
+
+      return args;
+    });
+  },
 };
